9c7680ac6170bbcaddbadcfc01dcfe35053acebe
Legat <zia-d@mail.ru>
2022-01-27 07:38:48 +0400

    feat: done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  
+  let restBrackets = str;
+  let chenged = true;
+  while (chenged) {
+    chenged = false;
+    bracketsConfig.forEach((pairOfBrackets) => {
+      let pairOfBracketsStr = pairOfBrackets[0] + pairOfBrackets[1];
+      let compare = restBrackets;
+      restBrackets = restBrackets.replaceAll(pairOfBracketsStr, '');
+      if (restBrackets !== compare) {
+        chenged = true;
+      }
+    });
+  }
+  return !restBrackets;
 }
